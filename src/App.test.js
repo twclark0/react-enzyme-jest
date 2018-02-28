3c438402e3d1be9f081230819398daa79f9908f7
twclark0 <tylerc644@gmail.com>
2018-02-27 23:07:29 -0700

    End of Lesson 3: Shallow render components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { configure } from 'enzyme'
+import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
+describe('<App />', () => {
+  it('should render App', () => {
+    const wrapper = shallow(<App />)
+  })
+})
