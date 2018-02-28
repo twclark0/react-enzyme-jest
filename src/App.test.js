e60142eb09c8ffd632d9a7987054a029699a0409
twclark0 <tylerc644@gmail.com>
2018-02-27 21:55:31 -0700

    Lesson 3: Shallow render component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() })
+
