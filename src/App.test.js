36dbf066676717e0492b6a57090a723558925099
twclark0 <tylerc644@gmail.com>
2018-03-04 14:14:42 -0700

    end of lesson 4

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,8 @@ import Adapter from "enzyme-adapter-react-16"
 configure({ adapter: new Adapter() })
 
 describe("<App />", () => {
-  it("should render App", () => {
-    const wrapper = shallow(<App />)
+  const wrapper = shallow(<App />)
+  it("h1 contains correct text", () => {
+    expect(wrapper.find('h1').text()).toBe('Welcome to React')
   })
 })
