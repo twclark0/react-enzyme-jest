50594866bcb5545958b1c86670929e013afe044d
twclark0 <tylerc644@gmail.com>
2018-03-08 10:34:38 -0700

    Test file cleanup

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Hello World
-        </p>
+        <p className="App-intro">Hello World</p>
       </div>
     )
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import { configure, shallow } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App, { Link } from './App'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 
 configure({ adapter: new Adapter() })
 
-describe("<App />", () => {
-  it("h1 contains correct text", () => {
+describe('<App />', () => {
+  it('h1 contains correct text', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('h1').text()).toBe('Welcome to React')
   })
